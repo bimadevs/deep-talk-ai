08a395f05311addf77ddc222c408e817134c3655
bimaj0206 <bimaj0206@gmail.com>
2025-04-03 16:25:54 +0000

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { Nav } from "@/components/Nav";
 import { cn } from "@/utils";
 
 export const metadata: Metadata = {
-  title: "Hume AI - EVI - Next.js Starter",
-  description: "A Next.js starter using Hume AI's Empathic Voice Interface",
+  title: "Deep Talk AI",
+  description: "Talk to AI like your psychiatrist",
 };
 
 export default function RootLayout({
